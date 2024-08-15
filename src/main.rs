7a14af3e378875a685c5b72778805e35961193de
Eleanor Young <developer@nausicaea.net>
2024-08-15 17:33:05 +0200

    Updated the project dependencies

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use tracing::debug;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
