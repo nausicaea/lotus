ac65421c30934181cc2dc4b4952f3a53c2b9747f
Eleanor Young <eleanor.young@unibas.ch>
2026-03-31 14:32:49 +0200

    refactor!: remove the static build Dockerfile and script because this wasn't ever used

--- build-static.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Builds the project in release mode into a statically linked binary (to be self-contained)
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-exec docker build -t lotus:latest $SCRIPT_DIR
